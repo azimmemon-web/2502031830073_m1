a11f3a7e0d476b71f25b069b5a81747de331aca7
azimmemon-web <azimmemon19@gmail.com>
2026-05-21 13:01:09 +0530

    day-7 Task

---
+++ day-9/FSWD/First_app/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import Hello from './Hello'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Day-9 MY FIRST REACT APP</h1>
+      <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ day-9/FSWD/First_app/my-app/src/Hello.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function Hello() {
+    let name = "Azim"
+
+    return (
+        <div>
+            <h2 className='test'>Hello, React {name}</h2>
+        </div>
+    )
+}
+
+export default Hello
